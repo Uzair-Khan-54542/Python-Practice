20cc6144469c257d204477c2a27254fdfa8d6627
Uzair-Khan-54542 <uzairkhan72004@gmail.com>
2026-02-21 22:29:30 +0500

    Concatination, merging and joining in DataFrames

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ df = pd.DataFrame({
     'cities': [np.nan, 'Lahore', 'Mansehra', 'Kiranchi', np.nan, 'Sialkot', np.nan]
 })
 
-print(df, '\n')
+# print(df, '\n')
 
 # print(df.isna().sum(), '\n')
 
@@ -22,5 +22,39 @@ print(df, '\n')
 # df.fillna(value = values)
 
 # print(df, '\n')
+
+
+
+
+# celebrities = pd.DataFrame({
+#     'celebrity_id': [1, 2, 3, 4, 5],
+#     'name': ['Kashi', 'Mana', 'Kalia', 'Billa', 'Jeda'],
+#     'city': ['Mansehra', 'Peshawar', 'Karachi', 'Multan', 'Lahore']
+# })
+
+# net_worth = pd.DataFrame({
+#     'celebrity_id': [1, 2, 3, 8, 5],
+#     'networth': [20000000, 45000000, 1800000, 9000000000, 990000000000],
+# })
+
+# print(celebrities, '\n\n', net_worth, '\n')
+
+# print(pd.merge(celebrities, net_worth, on = 'celebrity_id', how = 'outer'))
+# print(pd.concat([celebrities, net_worth]))
+
+# col_1 = pd.DataFrame({
+#     'age': [12, 15, 14, 12, 11, 13]
+# }, index = [0, 1, 2, 3, 4, 5])
+
+# col_2 = pd.DataFrame({
+#     'salary': [15000000, 20000000, 160000000, 158000000, 100000000, 2500000000]
+# }, index = [3, 4, 5, 6, 7, 8])
+
+
+# print(col_1.join(col_2))
+
+
+
+
 
 
