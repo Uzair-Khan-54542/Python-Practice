de2a61dcdd750fd26556869bbb00a74226d77468
Uzair-Khan-54542 <uzairkhan72004@gmail.com>
2026-02-24 15:20:51 +0500

    Extracted timestap, converted to dataetime object, calculated the duration in months and then stored  the final value in a new column.

--- index.py
+++ index.py
@@ -2,6 +2,9 @@
 
 import numpy as np 
 import pandas as pd 
+from datetime import datetime
+import time 
+from dateutil.relativedelta import relativedelta
 
 data = pd.read_csv(r'c:\Users\user\Downloads\anime.csv')
 
@@ -29,17 +32,39 @@ data['Episodes'] = data['Episodes'].str.replace(' eps', '')
 data['Episodes'] = data['Episodes'].astype(int)
 
 
+def extract_timestamp(txt):
+
+    start = txt.find(')')
+    duration = ""
+    for i in range(start + 1, start + 20):
+
+        duration += txt[i]
+    
+    return duration
+
+data['Total Time'] = data['Title'].apply(extract_timestamp)
+
+
+
+
+def extract_duration(txt):
+
+    start_str, end_str = txt.split(" - ")
+
+    start_date = datetime.strptime(start_str, '%b %Y')
+    end_date = datetime.strptime(end_str, '%b %Y')
+    r = relativedelta(end_date, start_date)
+    return r.years * 12 + r.months + 1
+
+    # return (end_date - start_date).days
+
+data['Duration(Months)'] = data['Total Time'].apply(extract_duration)
 
 
 
 
 
-
-
-
-
-
-
-
-# print(data.head())
+# print(data.loc[0]['Title'])
+print(data.head(), '\n')
+# print(data['Duration'].dtype, '\n')
 # print(data, '\n')
