5a33cde6042bd070d5cddc4d7c1dc58db319fae9
Uzair-Khan-54542 <uzairkhan72004@gmail.com>
2026-02-24 13:15:18 +0500

    Workign on a small dataset and performing feature extraction on a single columns. Key tasks are to extract episodes, time_stamp and other important info about TV series from a columns that contains all information in a single column.

--- index.py
+++ index.py
@@ -1,31 +1,35 @@
 
-import pandas as pd 
+
 import numpy as np 
+import pandas as pd 
+
+data = pd.read_csv(r'c:\Users\user\Downloads\anime.csv')
+
+
+# print(data.loc[1]['Title'])
+
+def extract_episodes(title):
+
+    episode = ""
+
+    start = title.find('(')
+    end = title.find(')')
+
+    for i in range(start + 1, end):
+
+        episode += title[i] 
+    
+    return episode
+
+
+data['Episodes'] = data['Title'].apply(extract_episodes)        
+
+data['Episodes'] = data['Episodes'].str.replace(' eps', '')
+
+data['Episodes'] = data['Episodes'].astype(int)
 
 
 
-df = pd.DataFrame({
-    'Date': pd.date_range('2023-01-01', periods = 20),
-    'Product': ['A', 'B', 'C', 'D'] * 5,
-    'Region': ['East', 'West', 'North', 'South'] * 5,
-    'Sales': np.random.randint(100, 1000, 20),
-    'Units': np.random.randint(10, 100, 20),
-    'Recp': ['John', 'Mary', 'Bob', 'Alice'] * 5
-})
-
-df['Month'] = df['Date'].dt.month_name()
-df['Quarter'] = 'Q' + df['Date'].dt.quarter.astype(str)
-
-print(f'\n\n{df}\n\n')
-
-pivot_1 = df.pivot_table(values = ['Sales', 'Units'], index = 'Region', columns = 'Product', aggfunc = 'median')
-pivot_2 = df.pivot_table(values = ['Sales', 'Units'], index = 'Region', columns = 'Product', aggfunc = 'mean')
-
-print(f'\n\nPivot Table # 1\n\n{pivot_1}\n\n')
-print(f'\n\nPivot Table # 2\n\n{pivot_2}\n\n')
-
-
-print(pd.crosstab(df['Region'], df['Product']))
 
 
 
@@ -34,3 +38,8 @@ print(pd.crosstab(df['Region'], df['Product']))
 
 
 
+
+
+
+# print(data.head())
+# print(data, '\n')
